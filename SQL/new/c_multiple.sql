5f6f3a43e3f951458b097a86982a3500269ab8cb
Gregory Krudysz <krudysz@gmail.com>
2012-03-21 19:14:42 -0700

    version 188ff

---
+++ SQL/new/c_multiple.sql
@@ -0,0 +1,15 @@
+alter table webct_c add column (formula2 varchar(256),
+				formula3 varchar(256),
+				formula4 varchar(256)
+);
+alter table webct_c add column (weight1 int(11),
+				weight2 int(11),
+				weight3 int(11),
+				weight4 int(11)
+);
+/*alter table webct_c rename column weight3  to weight3 */
+alter table webct_c add column (text1 varchar(256),
+				text2 varchar(256),
+				text3 varchar(256),
+				text4 varchar(256)
+);
